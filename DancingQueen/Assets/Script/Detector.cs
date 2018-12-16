e89029378c1c271b7ac0d44d8abc842bae7f13bd
Unknown <lucafloreau@akeonet.com>
2018-12-16 02:15:12 +0100

    menu + music test

--- DancingQueen/Assets/Script/Detector.cs
+++ DancingQueen/Assets/Script/Detector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Boo.Lang.Runtime;
 using UnityEngine;
 
 public class Detector : MonoBehaviour
@@ -12,11 +13,14 @@ public class Detector : MonoBehaviour
     [SerializeField] private GameObject textCoolPrefab;
     [SerializeField] private GameObject textPerfectPrefab;
     [SerializeField] private LayerMask raycastLayerMask;
+    private SpriteRenderer sprite;
+    [SerializeField] private Sprite dancingSprite;
 
     // Use this for initialization
     void Start ()
 	{
 	    boxCollider2D = GetComponent<BoxCollider2D>();
+	    sprite = GetComponentInChildren<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -25,51 +29,67 @@ public class Detector : MonoBehaviour
 	    if (Input.GetButtonDown(gameObject.tag))
 	    {
 	        Select();
+            sprite.color = Color.red;
 	    }
 
-	    foreach (var touch in Input.touches)
+	    if (Input.GetButtonUp(gameObject.tag))
 	    {
-	        if (touch.phase == TouchPhase.Began)
-	        {
-	            if (boxCollider2D.OverlapPoint(Camera.main.ScreenToWorldPoint(touch.position)))
-	            {
-                    Select();
-	            }
+	        sprite.color = Color.white;
+        }
+        foreach (var touch in Input.touches)
+	    {
+	       if (boxCollider2D.OverlapPoint(Camera.main.ScreenToWorldPoint(touch.position)))
+	       {
+	           if (touch.phase == TouchPhase.Began)
+	           {
+                  Select();
+	               sprite.color = Color.red;
+                }
+                if (touch.phase == TouchPhase.Ended)
+	           {
+	               Select();
+	               sprite.color = Color.white;
+                }
             }
         }
     }
 
     public void Select()
     {
-            if (GameManager.Instance.ArrowGameObject[gameObject.tag].Count > 0)
-                Debug.Log(GameManager.Instance.ArrowGameObject[gameObject.tag][0]);
+        if (GameManager.Instance.ArrowGameObject[gameObject.tag].Count > 0)
+        {
+            Debug.Log(GameManager.Instance.ArrowGameObject[gameObject.tag][0]);
             if (GameManager.Instance.ArrowGameObject[gameObject.tag][0].transform.position.y - gameObject.transform.position.y < -1)
             {
-                GameManager.Instance.AddScore(3);
+                GameManager.Instance.AddScore(0);
                 Instantiate(textNullPrefab, transform);
+                GameManager.Instance.Fail();
             }
             else if (GameManager.Instance.ArrowGameObject[gameObject.tag][0].transform.position.y - gameObject.transform.position.y < 0.5)
             {
                 GameManager.Instance.AddScore(3);
                 Instantiate(textPerfectPrefab, transform);
+                GameManager.Instance.Dancer.GetComponentInChildren<SpriteRenderer>().sprite = dancingSprite;
             }
             else if (GameManager.Instance.ArrowGameObject[gameObject.tag][0].transform.position.y - gameObject.transform.position.y < 1)
             {
-                GameManager.Instance.AddScore(3);
+                GameManager.Instance.AddScore(2);
                 Instantiate(textCoolPrefab, transform);
             }
             else if (GameManager.Instance.ArrowGameObject[gameObject.tag][0].transform.position.y - gameObject.transform.position.y < 2)
             {
-                GameManager.Instance.AddScore(3);
+                GameManager.Instance.AddScore(1);
                 Instantiate(textNicePrefab, transform);
             }
-            else if (GameManager.Instance.ArrowGameObject[gameObject.tag][0].transform.position.y - gameObject.transform.position.y >= 5)
+            else if (GameManager.Instance.ArrowGameObject[gameObject.tag][0].transform.position.y - gameObject.transform.position.y >= 2)
             {
-                GameManager.Instance.AddScore(3);
+                GameManager.Instance.AddScore(0);
                 Instantiate(textNullPrefab, transform);
+                GameManager.Instance.Fail();
             }
+
             Destroy(GameManager.Instance.ArrowGameObject[gameObject.tag][0]);
             GameManager.Instance.ArrowGameObject[gameObject.tag].RemoveAt(0);
+        }
     }
-    
 }
--- DancingQueen/Assets/Script/GameManager.cs
+++ DancingQueen/Assets/Script/GameManager.cs
@@ -26,23 +26,49 @@ public class GameManager : MonoBehaviour
     }
 
     [SerializeField] private TextMeshProUGUI scoreText;
+
+    [SerializeField] private GameObject dancer;
+    public GameObject Dancer
+    {
+        get { return dancer; }
+        set { dancer = value; }
+    }
+
+    [SerializeField] private Sprite fallingSprite;
+
+    [SerializeField] private AudioClip vinyle;
+    private AudioSource audioSource;
+    [SerializeField] private AudioSource music;
+
+    [SerializeField] private float musicVolume;
+
+    private FMOD.Studio.EventInstance musicFmod;
+    private FMOD.Studio.ParameterInstance musicVelo;
+    [SerializeField] private string musicEvent;
+
+
+
+
     // Use this for initialization
     void Start ()
 	{
 	    instance = this;
-
+	    audioSource = GetComponent<AudioSource>();
 	    arrowGameObject.Add("Left", new List<GameObject>());
 	    arrowGameObject.Add("Up", new List<GameObject>());
 	    arrowGameObject.Add("Down", new List<GameObject>());
 	    arrowGameObject.Add("Right", new List<GameObject>());
         scoreText.text = "Score : " + score;
-    }
+	    musicFmod = FMODUnity.RuntimeManager.CreateInstance(musicEvent);
+        musicFmod.getParameter("Parameter 1", out musicVelo);
+	    musicFmod.start();
+	}
 
     // Update is called once per frame
     void Update()
     {
-
-
+        //music.volume = musicVolume;
+        musicVelo.setValue(score/500f);
     }
 
     public void AddScore(int point)
@@ -51,5 +77,11 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score : " + score;
     }
 
-
+    public void Fail()
+    {
+        dancer.GetComponentInChildren<SpriteRenderer>().sprite = fallingSprite;
+        audioSource.Play();
+        GetComponent<Animator>().SetTrigger("Scratch");
+    }
+    
 }
